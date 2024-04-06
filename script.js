7222aaadeeedaf49d13f3dc46f5bb367b30c5820
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-04-06 10:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("body");
+  const message = document.createElement("p");
+  message.textContent = "DOM load success";
+  body.appendChild(message);
+});
