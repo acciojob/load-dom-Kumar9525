41e674584f989b61918ac62cc85cbcf5d09aead8
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-04-06 10:26:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
   message.textContent = "DOM load success";
   body.appendChild(message);
 });
+
+
+describe('Test Page', () => {
+  const baseUrl = 'http://example.com'; // Replace 'http://example.com' with your actual base URL
+
+  it('Should verify absence of child elements in the body tag', () => {
+    cy.visit(baseUrl);
+    cy.get('body').children().should('not.exist');
+  });
+});
+
