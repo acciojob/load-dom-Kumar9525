1c0a110b8617a6baf9b4af4800a9ad0987d6bf3b
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-04-10 12:08:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function () {
-  const body = document.querySelector("body");
-  const message = document.createElement("p");
-  message.textContent = "DOM load success";
-  body.appendChild(message);
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Ensure the body tag is initially empty:
+  document.body.innerHTML = "";
+
+  // Write the string to the body:
+  document.body.textContent = "DOM load success";
 });
-
-
-
